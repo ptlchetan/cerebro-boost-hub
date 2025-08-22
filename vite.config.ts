daa974a177a6bb660a171dea0de7162efa4116d5
Nishan <nishan.sompura@metizsoft.com>
2025-08-22 17:38:21 +0530

    set ignore file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: '/cerebro-boost-hub',
+  base: '/cerebro-boost-hub',
   server: {
     host: "::",
     port: 8080,
